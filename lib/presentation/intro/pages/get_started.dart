6c0343229b01057251bb285c8c5a4df661f1b7aa
Idhan Haidar Kurniawan <haidartin98@gmail.com>
2026-03-16 17:31:06 +0700

    commit kedua

---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotifyclone/common/widgets/button/basic_app_button.dart';
+import 'package:spotifyclone/core/configs/assets/app_images.dart';
+import 'package:spotifyclone/core/configs/assets/app_vectors.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 50, vertical: 40),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AppImages.introBG),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                Spacer(),
+                Text(
+                  "Enjoy the music you love",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 21),
+                Text(
+                  "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey,
+                    fontSize: 13,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                BasicAppButton(onPressed: () {}, title: "Get Started"),
+              ],
+            ),
+          ),
+          Container(color: Colors.black.withOpacity(0.5)),
+        ],
+      ),
+    );
+  }
+}
